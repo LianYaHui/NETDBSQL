efad2620c60610ed6214daeff7bafc5f69cbffc2
连亚辉 <634236658@qq.com>
2014-10-21 15:09:21 +0800

    init bug branch to fix limit

---
+++ FoxzyDBSql/FoxzyForMySql/MySqlDBOnExpression.cs
@@ -0,0 +1,57 @@
+﻿using FoxzyDBSql.DBInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FoxzyForMySql
+{
+    public class MySqlDBOnExpression : IDBOnExpression
+    {
+        AbsDbExpression currentExpression;
+
+
+        public AbsDbExpression On(string joinOnString)
+        {
+            this.Expression = joinOnString;
+            currentExpression._keyObject.Join.Add(this.Type + this.AsName.ToLower(), this);
+            return currentExpression;
+        }
+
+        public string TableName
+        {
+            get;
+            set;
+        }
+
+        public string AsName
+        {
+            get;
+            set;
+        }
+
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public string Expression
+        {
+            get;
+            set;
+        }
+
+        public IDBOnExpression Fill(AbsDbExpression exp)
+        {
+            this.currentExpression = exp;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            String table = (this.TableName == this.AsName ? this.TableName : string.Format("{0} as {1}", this.TableName, this.AsName));
+            return String.Format(" {0} {1} on {2}", this.Type, table, this.Expression);
+        } 
+    }
+}
--- FoxzyDBSql/FoxzyForMySql/MySqlExpression.cs
+++ FoxzyDBSql/FoxzyForMySql/MySqlExpression.cs
@@ -1,5 +1,6 @@
-﻿using FoxzyDBSql.DBInterface;
 using MySql.Data.MySqlClient;
+﻿using FoxzyDBSql.Common;
+using FoxzyDBSql.DBInterface;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,6 +11,18 @@ namespace FoxzyForMySql
 {
     public class MySqlExpression : AbsDbExpression
     {
+        private DbManage db;
+
+        public MySqlExpression(DbManage db, SqlExceType type)
+        {
+            if (db == null)
+                throw new Exception("DbManage is not null");
+
+            this.db = db;
+            this._keyObject.SqlType = type;
+        }
+
+
         public override AbsDbExpression Update(string tb)
         {
             if (String.IsNullOrEmpty(tb))
@@ -122,57 +135,95 @@ namespace FoxzyForMySql
 
         public override AbsDbExpression From(string tableName, string AsTableName = null)
         {
-            throw new NotImplementedException();
+            if (AsTableName == null)
+            {
+                this._keyObject.FromTable = tableName;
+            }
+            else
+            {
+                this._keyObject.FromTable = String.Format("{0} as {1}", tableName, AsTableName);
+            }
+
+            this._keyObject.Tables.Add(tableName, AsTableName);
+            return this;
         }
 
         public override AbsDbExpression From(string tablesql)
         {
-            throw new NotImplementedException();
+            foreach (String table in tablesql.Split(','))
+            {
+                if (table.IndexOf(" ") >= 0)
+                {
+                    String _tbName = table.Substring(0, table.IndexOf(" "));
+                    String _asName = table.Substring(table.LastIndexOf(" "));
+
+                    this._keyObject.Tables.Add(_tbName, _asName);
+                }
+                else
+                    this._keyObject.Tables.Add(table, null);
+            }
+
+            this._keyObject.FromTable = tablesql;
+            return this;
         }
 
         public override AbsDbExpression Select(IEnumerable<FoxzyDBSql.Common.DBSelectComponent> Component)
         {
-            throw new NotImplementedException();
+            this._keyObject.Selects = Component;
+            return this;
         }
 
         public override AbsDbExpression Select(string selectStr = null)
         {
-            throw new NotImplementedException();
+            this._keyObject.SelectStr = selectStr;
+            return this;
         }
 
         public override AbsDbExpression Into(string intoTable)
         {
-            throw new NotImplementedException();
+            this._keyObject.IntoTable = intoTable;
+            return this;
         }
 
         public override AbsDbExpression Where(string where)
         {
-            throw new NotImplementedException();
+            this._keyObject.WhereSql = where;
+            return this;
         }
 
         public override AbsDbExpression OrderBy(string field)
         {
-            throw new NotImplementedException();
+            this._keyObject.Sort.Add(field.ToLower(), true);
+            return this;
         }
 
         public override AbsDbExpression OrderBy(string field, string tableName)
         {
-            throw new NotImplementedException();
+            String col = String.Format("{0}.{1}", tableName, field);
+
+            this._keyObject.Sort.Add(col.ToLower(), true);
+            return this;
         }
 
         public override AbsDbExpression OrderByDesc(string field)
         {
-            throw new NotImplementedException();
+            this._keyObject.Sort.Add(field.ToLower(), false);
+            return this;
         }
 
         public override AbsDbExpression OrderByDesc(string field, string tableName)
         {
-            throw new NotImplementedException();
+            String col = String.Format("{0}.{1}", tableName, field);
+            this._keyObject.Sort.Add(col.ToLower(), false);
+            return this;
         }
 
         public override AbsDbExpression GropuBy(params string[] field)
         {
-            throw new NotImplementedException();
+            foreach (String f in field)
+                this._keyObject.GroupByField.Add(f);
+
+            return this;
         }
 
         public override AbsDbExpression SetParameter(params IDataParameter[] pars)
@@ -195,37 +246,161 @@ namespace FoxzyForMySql
 
         public override AbsDbExpression Having(string havingsql)
         {
-            throw new NotImplementedException();
+            this._keyObject.HavingSql = havingsql;
+            return this;
         }
 
         public override IDBOnExpression LeftJoin(string joinTable)
         {
-            throw new NotImplementedException();
+            joinTable = joinTable.Trim();
+            IDBOnExpression ex = new MySqlDBOnExpression();
+
+            ex.Type = "left join";
+
+            if (joinTable.IndexOf(" ") > 1)
+            {
+                ex.TableName = joinTable.Substring(0, joinTable.IndexOf(" "));
+                ex.AsName = joinTable.Substring(joinTable.LastIndexOf(" "));
+
+                this._keyObject.Tables.Add(ex.TableName, ex.AsName);
+            }
+            else
+            {
+                ex.TableName = joinTable;
+                ex.AsName = joinTable;
+
+                this._keyObject.Tables.Add(ex.TableName, null);
+            }
+
+            return ex.Fill(this);
         }
 
         public override IDBOnExpression RightJoin(string joinTable)
         {
-            throw new NotImplementedException();
+            joinTable = joinTable.Trim();
+            IDBOnExpression ex = new MySqlDBOnExpression();
+
+            ex.Type = "right join";
+
+            if (joinTable.IndexOf(" ") > 1)
+            {
+                ex.TableName = joinTable.Substring(0, joinTable.IndexOf(" "));
+                ex.AsName = joinTable.Substring(joinTable.LastIndexOf(" "));
+
+                this._keyObject.Tables.Add(ex.TableName, ex.AsName);
+            }
+            else
+            {
+                ex.TableName = joinTable;
+                ex.AsName = joinTable;
+
+                this._keyObject.Tables.Add(ex.TableName, null);
+            }
+
+            return ex.Fill(this);
         }
 
         public override IDBOnExpression InnerJoin(string joinTable)
         {
-            throw new NotImplementedException();
+            joinTable = joinTable.Trim();
+            IDBOnExpression ex = new MySqlDBOnExpression();
+
+            ex.Type = "inner join";
+
+            if (joinTable.IndexOf(" ") > 1)
+            {
+                ex.TableName = joinTable.Substring(0, joinTable.IndexOf(" "));
+                ex.AsName = joinTable.Substring(joinTable.LastIndexOf(" "));
+
+                this._keyObject.Tables.Add(ex.TableName, ex.AsName);
+            }
+            else
+            {
+                ex.TableName = joinTable;
+                ex.AsName = joinTable;
+
+                this._keyObject.Tables.Add(ex.TableName, null);
+            }
+
+            return ex.Fill(this);
         }
 
         public override string ToSql()
         {
+            StringBuilder sb_sql = new StringBuilder();
+
+            if (_keyObject.SqlType == SqlExceType.Select)
+            {
+                if (String.IsNullOrEmpty(_keyObject.FromTable))
+                    throw new Exception("没有制定表");
+
+                initSelect(sb_sql);
+                initInto(sb_sql);
+                initFrom(sb_sql);
+                initJoin(sb_sql);
+                initWhere(sb_sql);
+                initGroup(sb_sql);
+                initHaving(sb_sql);
+                initSort(sb_sql);
+                initLimit(sb_sql);
+
+                return sb_sql.ToString();
+            }
+            if (_keyObject.SqlType == SqlExceType.Update)
+            {
+                initUpdate(sb_sql);
+                initset(sb_sql);
+                initWhere(sb_sql);
+
+
+                return sb_sql.ToString();
+            }
+            if (_keyObject.SqlType == SqlExceType.Delete)
+            {
+                initDelete(sb_sql);
+                initWhere(sb_sql);
+
+                return sb_sql.ToString();
+            }
+
+            if (_keyObject.SqlType == SqlExceType.Insert)
+            {
+                initInsert(sb_sql);
+                initInsertColunmVal(sb_sql);
+
+
+
+                return sb_sql.ToString();
+            }
+
+
             throw new NotImplementedException();
         }
 
+        
+
         public override System.Data.DataSet ToDataSet()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return db.FillDataSet(this.ToSql(), this._keyObject.DataParameters);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public override object ExecuteScalar()
         {
-            throw new NotImplementedException();
+            try
+            {
+                return db.ExecuteScalar(this.ToSql(), this._keyObject.DataParameters);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public override int ExecuteNonQuery()
@@ -247,12 +422,224 @@ namespace FoxzyForMySql
 
         public override AbsDbExpression Top(int count)
         {
-            throw new NotImplementedException();
+            throw new Exception("Mysql不支持Top语句");
         }
 
         public override AbsDbExpression RowPagination(int beginRowNumber, int endRowNumber)
         {
             throw new NotImplementedException();
         }
+
+
+
+
+        #region 私有方法
+        private void initSelect(StringBuilder sb_sql)
+        {
+            List<String> select_sql = new List<string>();
+            sb_sql.Append("select ");
+
+            if (_keyObject.Top != 0)
+                sb_sql.AppendFormat("top {0} ", _keyObject.Top);
+
+            //Select
+            if (!String.IsNullOrEmpty(_keyObject.SelectStr) || _keyObject.Selects != null)
+            {
+                if (_keyObject.Selects != null)
+                {
+                    foreach (var c in _keyObject.Selects)
+                    {
+                        String tableName = c.TableName;
+                        foreach (var col in c.Colunms)
+                        {
+                            String _select = String.IsNullOrEmpty(col.AsName) ?
+                                String.Format("$${0}", col.ColunmName) :
+                                String.Format("$${0} as {1}", col.ColunmName, col.AsName);
+
+                            if (String.IsNullOrEmpty(tableName)) _select = _select.Replace("$$", "");
+                            else _select = _select.Replace("$$", tableName + ".");
+
+                            select_sql.Add(_select);
+                        }
+                    }
+                    sb_sql.AppendFormat(String.Join(",", select_sql.ToArray()));
+                    return;
+                }
+
+                if (!String.IsNullOrEmpty(_keyObject.SelectStr))
+                {
+                    sb_sql.Append(_keyObject.SelectStr);
+                }
+            }
+            else
+            {
+                //没有定义字段，则查询出表的所有字段　［ｔａｂｌｅ］．＊
+                //多表
+                //连接
+                foreach (var tb in _keyObject.Tables)
+                {
+                    if (String.IsNullOrEmpty(tb.Value))
+                        select_sql.Add(tb.Key + ".*");
+                    else
+                        select_sql.Add(tb.Value + ".*");
+                }
+
+                sb_sql.AppendLine(String.Join(",", select_sql.ToArray()));
+            }
+        }
+
+        void initInto(StringBuilder sb_sql)
+        {
+            if (!String.IsNullOrEmpty(_keyObject.IntoTable))
+            {
+                sb_sql.AppendFormat(" into {0}", _keyObject.IntoTable);
+            }
+        }
+
+
+        void initFrom(StringBuilder sb)
+        {
+            sb.AppendFormat(" from {0}", this._keyObject.FromTable);
+        }
+
+        void initWhere(StringBuilder sb)
+        {
+            if (!String.IsNullOrEmpty(this._keyObject.WhereSql))
+            {
+                sb.Append(" where ");
+                sb.Append(this._keyObject.WhereSql);
+            }
+        }
+
+        void initSort(StringBuilder sb)
+        {
+            if (this._keyObject.Sort.Count == 0)
+                return;
+
+            List<String> orderSql = new List<string>();
+            sb.Append(" order by ");
+
+            foreach (String key in _keyObject.Sort.Keys)
+            {
+                if ((bool)(_keyObject.Sort[key]))
+                {
+                    orderSql.Add(String.Format("{0} asc", key));
+                }
+                else
+                {
+                    orderSql.Add(String.Format("{0} desc", key));
+                }
+            }
+
+            sb.Append(String.Join(",", orderSql.ToArray()));
+        }
+
+        void initJoin(StringBuilder sb)
+        {
+            if (this._keyObject.Join.Count == 0)
+                return;
+
+            foreach (String key in _keyObject.Join.Keys)
+            {
+                var onExp = _keyObject.Join[key] as IDBOnExpression;
+
+                sb.Append(onExp.ToString());
+            }
+
+        }
+
+        void initGroup(StringBuilder sb)
+        {
+            if (this._keyObject.GroupByField.Count == 0)
+                return;
+
+            sb.AppendFormat(" group by {0}", String.Join(",", this._keyObject.GroupByField.ToArray()));
+        }
+
+        void initHaving(StringBuilder sb)
+        {
+            if (!String.IsNullOrEmpty(this._keyObject.HavingSql))
+            {
+                sb.Append(" having ");
+                sb.Append(this._keyObject.HavingSql);
+            }
+        }
+
+        void initUpdate(StringBuilder sb)
+        {
+            sb.AppendFormat("update {0} ", this._keyObject.UpdateTable);
+        }
+
+        void initset(StringBuilder sb)
+        {
+            List<String> vals = this._keyObject.Set.Values.OfType<String>().ToList();
+
+            foreach (System.Collections.DictionaryEntry d in this._keyObject.OperateObject)
+            {
+                vals.Add(d.Key + "= ?" + Convert.ToString(d.Key));
+                this._keyObject.DataParameters.Add(new MySqlParameter("?" + d.Key, d.Value));
+            }
+
+            if (vals.Count == 0)
+                throw new Exception("至少制定一个Set可供更新");
+
+            sb.AppendFormat("set {0}", String.Join(",", vals.ToArray()));
+        }
+
+        private void initDelete(StringBuilder sb)
+        {
+            sb.AppendFormat("delete {0} ", this._keyObject.DeleteTable);
+        }
+
+        private void initInsert(StringBuilder sb_sql)
+        {
+            if (String.IsNullOrEmpty(this._keyObject.InsertTable))
+                throw new Exception("insert 表为空");
+
+            sb_sql.AppendFormat("insert {0} ", this._keyObject.InsertTable);
+        }
+
+        void initInsertColunmVal(StringBuilder sb_sql)
+        {
+            if (_keyObject.InsertColoums.Count > 0)
+            {
+                sb_sql.AppendFormat("({0}) ", String.Join(",", _keyObject.InsertColoums.ToArray()));
+
+                initSelect(sb_sql);
+                initFrom(sb_sql);
+                initJoin(sb_sql);
+                initWhere(sb_sql);
+                initGroup(sb_sql);
+                initHaving(sb_sql);
+                initSort(sb_sql);
+            }
+            else if (_keyObject.OperateObject.Count > 0)
+            {
+                List<String> clo = new List<string>();
+                List<String> vals = new List<string>();
+
+                foreach (System.Collections.DictionaryEntry d in this._keyObject.OperateObject)
+                {
+                    clo.Add(Convert.ToString(d.Key));
+                    vals.Add("@" + Convert.ToString(d.Key));
+
+                    this._keyObject.DataParameters.Add(new MySqlParameter("?" + d.Key, d.Value));
+                }
+
+                sb_sql.AppendFormat("({0}) values ({1})",
+                    String.Join(",", clo.ToArray()),
+                    String.Join(",", vals.ToArray()));
+            }
+            else
+                throw new Exception("这个你还是看下ToSql就知道了");
+        }
+
+        private void initLimit(StringBuilder sb_sql)
+        {
+            sb_sql.AppendFormat("limit {0}");
+        }
+
+        #endregion
+
     }
 }

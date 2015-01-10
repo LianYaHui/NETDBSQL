74da3e1ce8b89df9ac051a96c17c5b6ee4a06d3f
连亚辉 <634236658@qq.com>
2015-01-10 19:38:51 +0800

    lian

---
+++ FoxzyDBSql/FoxzyDBSqlTests/SqlServer/SqlManageUtilTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoxzyDBSql.SqlServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace FoxzyDBSql.SqlServer.Tests
+{
+    [TestClass()]
+    public class SqlManageUtilTests
+    {
+        [TestMethod()]
+        public void SqlManageUtilTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CloneParameterTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void OpenConncetionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteDataReaderTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteNonQueryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteScalarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ExecuteScalarTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FillDataSetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateSelectTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateUpdateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateDeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateInsertTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreatePaginationTest()
+        {
+            SqlManageUtil db = new SqlManageUtil("Data Source=.;Initial Catalog=HRM_XX;Integrated Security=True");
+            int c = 0;
+
+            db.CreatePagination()
+                .Set("SELect * from xx_employee", null)
+                .Pagination(2, 5, out c, "ID");
+
+            Assert.AreEqual(c, 24);
+
+        }
+    }
+}

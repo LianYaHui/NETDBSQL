eb5df4380e173507933ab918275790b2ac06064c
连亚辉 <634236658@qq.com>
2015-03-12 16:44:22 +0800

    新增模型化的转换类

---
+++ FoxzyDBSql/FoxzyDBSql/Common/SqlConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace FoxzyDBSql.Common
+{
+    public static class SqlConverter
+    {
+        public static List<T> ToList<T>(DataTable table, this Func<DataRow, T> render)
+        {
+            if (render == null)
+                throw new NullReferenceException("render");
+
+            List<T> data = new List<T>();
+
+            foreach (DataRow row in table.Rows)
+            {
+                data.Add(render.Invoke(row));
+            }
+
+            return data;
+        }
+    }
+}
